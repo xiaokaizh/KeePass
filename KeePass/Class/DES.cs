0ba91f8ce7de4cf7d41bbc8304bacd5cc229aa6f
xiaokaizh <xiaokaizh@163.com>
2016-02-28 15:07:45 +0800

    complete except UI

---
+++ KeePass/Class/DES.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeePass.Class
+{
+
+    /********************************************************************************
+    ** Author： Xiaokai Zh
+    ** Created：2016-02-27
+    ** Desc：DES 算法 加密/解密 静态类(密钥，且必须为8位)
+    *********************************************************************************/
+
+    public static class DES
+    {
+        /// <summary>
+        /// 进行DES加密。
+        /// </summary>
+        /// <param name="pToEncrypt">要加密的字符串。</param>
+        /// <param name="sKey">密钥，且必须为8位。</param>
+        /// <returns>以Base64格式返回的加密字符串。</returns>
+        public static bool Encrypt(string pToEncrypt, string sKey,out string encryptString)
+        {
+            //如果出错，出错信息将储存于encryptString返回
+            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+            {
+                if (!des.ValidKeySize(sKey.Length))
+                {
+                    encryptString = "Error:Key is not valid size";
+                    return false;
+                }
+                   
+                bool bl = des.ValidKeySize(sKey.Length);
+                byte[] inputByteArray = Encoding.UTF8.GetBytes(pToEncrypt);
+                des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                try
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inputByteArray, 0, inputByteArray.Length);
+                        cs.FlushFinalBlock();
+                        cs.Close();
+                    }
+                    encryptString = Convert.ToBase64String(ms.ToArray());
+                    ms.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    encryptString = e.ToString();
+                    return false;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 进行DES解密。
+        /// </summary>
+        /// <param name="pToDecrypt">要解密的以Base64</param>
+        /// <param name="sKey">密钥，且必须为8位。</param>
+        /// <returns>已解密的字符串。</returns>
+        public static bool Decrypt(string pToDecrypt, string sKey, out string decryptString)
+        {
+            byte[] inputByteArray;
+            try
+            {
+                inputByteArray = Convert.FromBase64String(pToDecrypt);
+            }
+            catch (Exception e)
+            {
+                decryptString = e.ToString();
+                return false;
+            }
+
+            using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+            {
+                if (!des.ValidKeySize(sKey.Length))
+                {
+                    decryptString = "Error:Key is not valid size";
+                    return false;
+                }
+                des.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+                des.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                try
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inputByteArray, 0, inputByteArray.Length);
+                        cs.FlushFinalBlock();
+                        cs.Close();
+                    }
+                    decryptString = Encoding.UTF8.GetString(ms.ToArray());
+                    ms.Close();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    decryptString = e.ToString();
+                    return false;
+                }
+            }
+        }  
+    }
+}
---
+++ KeePass/Class/Md5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeePass.Class
+{
+
+    /********************************************************************************
+    ** Author： Xiaokai Zh
+    ** Created：2016-02-27
+    ** Desc：MD5 算法加密
+    *********************************************************************************/
+
+    public static class Md5
+    {
+        public static bool Md5Encrypt(string needEncryptString,out string  encryptString)
+        {
+            try
+            {
+                //通用编码格式utf-8,不同的编码格式的MD5码不一样，这里推荐使用utf-8
+                byte[] result = Encoding.UTF8.GetBytes(needEncryptString);
+                MD5 md = new MD5CryptoServiceProvider();
+                byte[] output = md.ComputeHash(result);
+                encryptString = BitConverter.ToString(output).Replace("-", "");
+                return true;
+            }
+            catch (Exception e)
+            {
+                encryptString = e.ToString();
+                return false;
+            }
+        }
+
+    }
+}
