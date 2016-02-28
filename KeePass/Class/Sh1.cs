a39ab39efccd84475594cefc9556c426b2171709
xiaokaizh <xiaokaizh@163.com>
2016-02-28 15:05:06 +0800

    https://github.com/xiaokaizh/KeePass.git

---
+++ KeePass/Class/Sh1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeePass.Class
+{
+
+    /********************************************************************************
+    ** Author： Xiaokai Zh
+    ** Created：2016-02-27
+    ** Desc：SHA-1密码算法 加密
+    *********************************************************************************/
+
+    public static class Sh1
+    {
+        public static bool  SHA1(string needEncryptString,out string  encryptString)
+        {
+            try
+            {
+                byte[] result = Encoding.UTF8.GetBytes(needEncryptString);
+                System.Security.Cryptography.SHA1 sh1 = System.Security.Cryptography.SHA1.Create();
+                sh1.ComputeHash(result);
+                encryptString = Convert.ToBase64String(sh1.Hash);
+                return true;
+            }
+            catch (Exception e)
+            {
+                encryptString = e.ToString();
+                return false;
+            }
+        } 
+    }
+}
--- KeePass/MainWindow.xaml.cs
+++ KeePass/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KeePass.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,9 @@ using System.Windows.Shapes;
 
 namespace KeePass
 {
+    //1.保存常用密码，记录密码使用时间和测试密码强度
+    //2.根据规则生成密码
+    // 1.加密的规则  2.文件的本地保存
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
@@ -23,6 +27,8 @@ namespace KeePass
         public MainWindow()
         {
             InitializeComponent();
+            string d;
+            bool dl = Sh1.SHA1("赵晓凯是个大笨蛋", out d);
         }
     }
 }
